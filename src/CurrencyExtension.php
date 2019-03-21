dd3fce841126b2971833ea25ca9285c0cf7cfcd0
Rixafy <rixafy@gmail.com>
2019-03-21 22:25:26 +0100

    Add config service to container builder with default values

--- src/CurrencyExtension.php
+++ src/CurrencyExtension.php
@@ -4,7 +4,24 @@ declare(strict_types=1);
 
 namespace Archette\Currency;
 
+use Rixafy\Currency\CurrencyConfig;
+
 class CurrencyExtension extends \Nette\DI\CompilerExtension
 {
+    private $defaults = [
+        'apiKey' => 'undefined',
+        'apiService' => 'fixer',
+        'baseCurrency' => 'EUR'
+    ];
 
+    public function loadConfiguration()
+    {
+        $this->validateConfig($this->defaults);
+
+        $this->getContainerBuilder()->addDefinition($this->prefix('currencyConfig'))
+            ->setFactory(CurrencyConfig::class)
+            ->addSetup('setApiKey', [$this->config['apiKey']])
+            ->addSetup('setApiService', [$this->config['apiService']])
+            ->addSetup('setBaseCurrency', [$this->config['baseCurrency']]);
+    }
 }
