72b2ce1a363b099369d17c05a53caa46aef21353
Rixafy <rixafy@gmail.com>
2019-05-11 23:06:53 +0200

    Add return types to methods

--- src/CurrencyExtension.php
+++ src/CurrencyExtension.php
@@ -32,14 +32,14 @@ class CurrencyExtension extends CompilerExtension
 		]);
 	}
 
-    public function beforeCompile()
+    public function beforeCompile(): void
     {
     	/** @var ServiceDefinition $serviceDefinition */
         $serviceDefinition = $this->getContainerBuilder()->getDefinitionByType(AnnotationDriver::class);
         $serviceDefinition->addSetup('addPaths', [['vendor/rixafy/currency']]);
     }
 
-    public function loadConfiguration()
+    public function loadConfiguration(): void
     {
         $this->getContainerBuilder()->addDefinition($this->prefix('currencyConfig'))
             ->setFactory(CurrencyConfig::class, [
